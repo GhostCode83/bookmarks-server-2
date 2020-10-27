cdee1fd0c0a4caf6299481b8c164999294a62a1a
Randy Douglas <randy.e.douglas@gmail.com>
2020-10-27 13:28:26 -0400

    seed added

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,27 @@
+INSERT INTO bookmarks (title, url, description, rating) 
+VALUES 
+('Web Development Roadmaps', 
+'https://www.w3schools.com/whatis/', 
+'Every Web Developer must have a basic understanding of HTML, CSS, and JavaScript.', 
+5
+), 
+('Web Development', 
+'https://en.wikipedia.org/wiki/Web_development', 
+'Web development is the work involved in developing a Web site for the Internet (World Wide Web) or an intranet (a private network).', 
+1
+), 
+('Web Development', 
+'https://www.codecademy.com/learn/paths/web-development', 
+'Go from no-code to an in-demand junior web developer, at a fraction of the cost of a bootcamp. Start with the front-end by learning HTML, CSS, and JavaScript. Then, master the back-end and APIs to round out your full-stack skills.', 
+5
+), 
+('How To Become A Web Developer — Everything You Need To Know', 
+'https://careerfoundry.com/en/blog/web-development/what-does-it-take-to-become-a-web-developer-everything-you-need-to-know-before-getting-started/', 
+'Technology plays a huge role in our daily lives, from the simplest of apps to the most groundbreaking inventions. Every website or piece of software that we encounter has been built by a web developer — but what exactly is web development, and what does a web developer do?', 
+4
+), 
+('Learn web development', 
+'https://developer.mozilla.org/en-US/docs/Learn', 
+'Welcome to the MDN learning area. This set of articles aims to provide complete beginners to web development with all that they need to start coding websites.', 
+3
+)
